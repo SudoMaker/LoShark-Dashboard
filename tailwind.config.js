26fadd4e8279b7de37424b76dd1c3c08645c6492
Yukino Song <nutosservice@gmail.com>
2025-06-16 03:59:12 +0800

    Prepare for upgrade

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,7 @@
-import daisyui from 'daisyui'
-
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
 		extend: {}
-	},
-	plugins: [daisyui],
-	daisyui: {
-		// themes: ['dark', 'light', 'cyberpunk'],
-		themes: true
 	}
 }
